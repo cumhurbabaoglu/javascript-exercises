b6453a4262a10a1f2c8bcfeafeef6201f7410be3
Cumhur Babaoğlu <cbabaoglu.dev@gmail.com>
2026-01-20 03:06:13 +0300

    Write the rest of the function

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
 const removeFromArray = function(arr, ...args) {
+    for (let arg of args) {
+        let index = arr.indexOf(arg);
+        arr.splice(index, 1);
+    }
+    return arr;
 };
 
 // Do not edit below this line
