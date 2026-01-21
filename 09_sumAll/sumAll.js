1350897d358f0b59654138d0c5554f12d0740ced
Cumhur Babaoğlu <cbabaoglu.dev@gmail.com>
2026-01-22 01:46:36 +0300

    Add else if to deal with the case when either number is negative and to throw error message.

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -12,6 +12,8 @@ const sumAll = function(a, b) {
         }
         const result = array.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
         return result;
+    } else if (a < 0 || b < 0) {
+        throw new Error("Please make sure that both numbers are positive.");
     }
 };
 
