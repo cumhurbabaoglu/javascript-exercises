885bec5e0a051457ca5fe006bfee86245b22fa5d
Cumhur Babaoğlu <cbabaoglu.dev@gmail.com>
2026-01-22 01:24:39 +0300

    Use a for loop to add numbers between and including a and b to the array starting from the smaller value and going up to the bigger one at each iteration.

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -2,7 +2,9 @@ const sumAll = function(a, b) {
     const array = [];
     if (a > 0 && b > 0) {
         if (a < b) {
-            
+            for (let i = a; i <= b; i++) {
+                array.push(i);
+            }
         }
     }
 
