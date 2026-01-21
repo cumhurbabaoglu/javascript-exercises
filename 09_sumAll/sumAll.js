ce1b58839c1281edb4f9a5c48382751d6797b873
Cumhur Babaoğlu <cbabaoglu.dev@gmail.com>
2026-01-22 02:14:19 +0300

    Rearrange the conditions and make the code cleaner

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,20 +1,19 @@
 const sumAll = function(a, b) {
     const array = [];
-    if (a > 0 && b > 0) {
-        if (a < b) {
-            for (let i = a; i <= b; i++) {
-                array.push(i);
-            }
-        } else if (a > b) {
-            for (let i = b; i <= a; i++) {
-                array.push(i);
-            }
-        }
-        const result = array.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
-        return result;
-    } else if (a < 0 || b < 0 || !Number.isInteger(a) || !Number.isInteger(b)) {
+    if (a < 0 || b < 0 || !Number.isInteger(a) || !Number.isInteger(b)) {
         return "ERROR";
     }
+    if (a < b) {
+        for (let i = a; i <= b; i++) {
+                array.push(i);
+        }
+    } else if (a > b) {
+        for (let i = b; i <= a; i++) {
+                array.push(i);
+        }
+    }
+        const result = array.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+        return result;
 };
 
 // Do not edit below this line
