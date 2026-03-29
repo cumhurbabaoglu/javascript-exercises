83098f27bfdd76146eb20a2e9240508019feee7a
Cumhur Babaoğlu <cbabaoglu.dev@gmail.com>
2026-03-29 17:40:04 +0300

    Write all the functions

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(...arr) {
+	return arr.reduce((acc, num) => acc + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(...arr) {
+  return arr.reduce((acc, num) => acc * num, 1)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	 if (num === 0) {
+            return 1;
+        }
+   if (num < 0) {
+            throw new Error("Please enter a positive number");
+        }
+   let result = 1;
+   for (let i=1; i <= num; i++) {
+            result *= i;
+        }
+    return result;
 };
 
 // Do not edit below this line
