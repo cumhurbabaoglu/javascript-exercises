28e993faf7d1c68ff37bcaff7e9afb8565658c9a
Cumhur Babaoğlu <cbabaoglu.dev@gmail.com>
2026-03-30 19:00:10 +0300

    Write the function body for palindromes exercise

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const array = string.toLowerCase()
+                         .split("")
+                        .filter(letter => letter !== "," && letter !== " ");
 
+    for (let i = 0; i < array.length / 2; i++) {
+        if (array[i] !== array.at(-i - 1)) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
