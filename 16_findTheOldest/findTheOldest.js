c531e6175e09fe134654327a3f61722dc0752104
Cumhur Babaoğlu <cbabaoglu.dev@gmail.com>
2026-04-01 01:01:57 +0300

    Write the function body for findTheOldest exercise

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const currentYear = new Date().getFullYear();
+    return people.reduce((oldestPerson, currentPerson) => {
+    let currentPersonAge;
+    let oldestPersonAge;
+    if (!currentPerson.yearOfDeath) {
+      currentPersonAge = currentYear - currentPerson.yearOfBirth;
+    } else {
+      currentPersonAge = currentPerson.yearOfDeath - currentPerson.yearOfBirth;
+    }
+    if (!oldestPerson.yearOfDeath) {
+      oldestPersonAge = currentYear - oldestPerson.yearOfBirth;
+    } else {
+      oldestPersonAge = oldestPerson.yearOfDeath - oldestPerson.yearOfBirth;
+    }
+    return currentPersonAge > oldestPersonAge ? currentPerson : oldestPerson;
+  });
 };
 
 // Do not edit below this line
