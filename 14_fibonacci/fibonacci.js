65a78659218b480248a14f32df69d1b31799c5fa
Cumhur Babaoğlu <cbabaoglu.dev@gmail.com>
2026-03-31 16:02:08 +0300

    Add another if statement for cases where num is less than 0

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -2,6 +2,9 @@ const fibonacci = function(num) {
   if (num === 0) {
     return 0;
   }
+  if (num < 0) {
+    return "OOPS";
+  }
  // create an array and seed it with initial values
  const fibonacciSequence = [1, 1];
     for (let i = 2; num > fibonacciSequence.length; i++) {
